6e1bda8bba312507612e50a18ec709bc17eb3751
chronosacaria <chronos.droid@gmail.com>
2023-11-09 11:08:03 -0500

    1.20 Update, Translation Capabilities  - Updated `minecraft_version` to Minecraft 1.20.1  - Updated `yarn_mappings` to `1.20.1+build.10`  - Updated `loader_version` to `0.14.24`  - Updated `fabric_version` to `0.90.7+1.20.1`  - Updated `fabric-loom` to `1.4-SNAPSHOT`  - Updated Fabric Permissions API to `0.3-SNAPSHOT`  - Updated Player Ability Lib to `1.8.0`  - Updated Gradle to 8.3  - Updated Ladysnakes Mods Maven url  - Added translation capability for command feedback text  - Added translation capability for command input    - e.g. `/pvp <PLAYER_NAME> activado` == `/pvp <PLAYER_NAME> enable`; Autocomplete for non-English input does not work yet  - Added Spanish (Spain) Translation  - Optimised Imports

--- src/main/java/me/braunly/togglepvp/Perms.java
+++ src/main/java/me/braunly/togglepvp/Perms.java
@@ -1,9 +1,5 @@
 package me.braunly.togglepvp;
 
-import me.lucko.fabric.api.permissions.v0.PermissionCheckEvent;
-import net.fabricmc.fabric.api.util.TriState;
-import net.minecraft.command.CommandSource;
-
 public class Perms {
     public static final class Registry {
         // Access to /pvp command
