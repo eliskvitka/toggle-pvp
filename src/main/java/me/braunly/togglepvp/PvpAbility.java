305ae6de1496d9bdeea1b3a8b94421c8d32579cb
Carlos Ferreras <carlos@carlosferreras.com>
2024-08-14 17:07:49 +0200

    Updated to 1.21

--- src/main/java/me/braunly/togglepvp/PvpAbility.java
+++ src/main/java/me/braunly/togglepvp/PvpAbility.java
@@ -5,7 +5,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 
 public class PvpAbility {
-    private static final Identifier id = new Identifier(TogglePvp.MOD_ID, "pvp_state");
+    private static final Identifier id = Identifier.of(TogglePvp.MOD_ID, "pvp_state");
     private static final PlayerAbility PVP_ABILITY = Pal.registerAbility(id, SimpleAbilityTracker::new);
     private static final AbilitySource abilitySource = Pal.getAbilitySource(id);
 
