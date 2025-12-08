64585ec39ff9e0b0d81e275f858bf0ec6c4bcfeb
Elis Kvitka <git@kvitka.me>
2025-12-08 13:14:45 +0000

    fix(mixin): save pvp state after death and changing worlds
    Meta: Fixes #3

--- src/main/java/me/kvitka/togglepvp/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/me/kvitka/togglepvp/mixin/ServerPlayerEntityMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayerEntity.class)
@@ -19,5 +20,10 @@ public class ServerPlayerEntityMixin {
 
 		// NOTE: Ignoring result from teammates check
 		cir.setReturnValue(isVictimPvpEnabled && isAttackerPvpEnabled);
+	}
+
+	@Inject(at = @At("TAIL"), method = "copyFrom")
+	private void onCopy(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo ci) {
+		PvpAbility.set((PlayerEntity)(Object)this, PvpAbility.get(oldPlayer));
 	}
 }
